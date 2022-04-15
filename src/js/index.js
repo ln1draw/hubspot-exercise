76bfcd14368d03d913de1b9c01b22c0404c50c63
ellen wondra <ellenfromillinois@gmail.com>
2022-04-15 16:34:10 -0700

    adds my work

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,222 @@
 import '../styles/index.css';
+import '../styles/index.scss';
 
 console.log('App Ready');
+
+var GENRES = {};
+var YEARS = {};
+var FILTER = { genres: [], years: [], mediaType: '' };
+const MEDIADATA = require('./data/data.json').media.sort(alphabeticalByTitle);
+
+window.onload = function() {
+	// inefficient!! but, time constraints. small one-time cost.
+	MEDIADATA.forEach(item => {
+		buildGenreAndYear(item);
+	});
+
+	buildFilters();
+	buildAllMediaCards();
+}
+
+// FUNCTIONS ARE ALPHABETICAL
+
+// only sort-of alphabetical--a better function would handle things like articles (A, The, etc)
+function alphabeticalByTitle(a, b) {
+	var titleA = a['title'].toUpperCase();
+	var titleB = b['title'].toUpperCase();
+	return (titleA < titleB) ?  -1 : (titleA > titleB) ? 1 : 0;
+}
+
+function buildAllMediaCards() {
+	var contentDiv = document.getElementById("media-contents");
+	contentDiv.innerHTML = "";
+
+	buildFilteredMedia().forEach(item => {
+		contentDiv.appendChild(buildMediaCard(item));
+	})
+}
+
+// builds the media data block and applies appropriate filters
+function buildFilteredMedia() {
+	var mediaData = MEDIADATA;
+
+	if (FILTER['mediaType'] !== '') {
+		mediaData = mediaData.filter(item => FILTER['mediaType'] === item.type);
+	}
+
+	if (FILTER['years'].length > 0) {
+		mediaData = mediaData.filter(item => FILTER['years'].indexOf(item.year) > -1);
+	}
+
+	if (FILTER['genres'].length > 0) {
+		mediaData = mediaData.filter(function(item) {
+			var selecteGenre = false;
+			item['genre'].forEach(genre => {
+				if (FILTER['genres'].indexOf(genre) > -1) {
+					selecteGenre = true;
+				}
+			});
+			return selecteGenre;
+		});
+	}
+
+	return mediaData;
+}
+
+// could be DRYer; at this point not worth the time to change
+function buildFilters() {
+	var movieButton = document.getElementById('media-movie');
+	movieButton.addEventListener('change', onFilterChange, false);
+
+	var bookButton = document.getElementById('media-book');
+	bookButton.addEventListener('change', onFilterChange, false);
+
+	var genresBox = document.getElementById("genre-box");
+	genresBox.style.display = 'none';
+	document.getElementById('genre-toggle').addEventListener('click', function(e) {
+		genresBox.style.display = (genresBox.style.display === 'none') ? 'inline-block' : 'none'
+	})
+
+	Object.keys(GENRES).sort().forEach(genre => {
+		var inputWrapper = document.createElement('li');
+		inputWrapper.className = 'input-wrapper';
+
+		var genreInput = document.createElement('input');
+		genreInput.type = 'checkbox';
+		genreInput.name = 'genre';
+		genreInput.id = genre;
+		genreInput.value = genre;
+		genreInput.addEventListener('change', onFilterChange, false);
+
+		var genreLabel = document.createElement('label');
+		genreLabel.htmlFor = genre;
+		genreLabel.innerHTML = genre;
+		genreLabel.className = 'uppercase';
+		genreLabel.id = genre + '-label';
+
+		inputWrapper.appendChild(genreInput);
+		inputWrapper.appendChild(genreLabel);
+		genresBox.appendChild(inputWrapper);
+	})
+
+	var yearBox = document.getElementById('year-box');
+	yearBox.style.display = 'none';
+	document.getElementById('year-toggle').addEventListener('click', function(e) {
+		yearBox.style.display = (yearBox.style.display === 'none') ? 'inline-block' : 'none'
+	})
+
+	Object.keys(YEARS).sort().reverse().forEach(year => {
+		var inputWrapper = document.createElement('li');
+		inputWrapper.className = 'input-wrapper';
+
+		var yearInput = document.createElement('input');
+		yearInput.type = 'checkbox';
+		yearInput.name = 'year';
+		yearInput.id = year;
+		yearInput.value = year;
+		yearInput.addEventListener('change', onFilterChange, false);
+
+		var yearLabel = document.createElement('label');
+		yearLabel.htmlFor = year;
+		yearLabel.innerHTML = year;
+		yearLabel.className = 'uppercase';
+
+		inputWrapper.appendChild(yearInput);
+		inputWrapper.appendChild(yearLabel);
+		yearBox.appendChild(inputWrapper);
+	})
+
+	var form = document.getElementById("filter-form");
+
+	form.addEventListener("reset", (event) => {
+	  event.preventDefault();
+	  resetFilters();
+	});
+}
+
+function buildGenreAndYear(item) {
+	item.genre.forEach(item => {
+		GENRES[item] = true
+	})
+	YEARS[item.year] = true
+}
+
+function buildMediaCard(itemData) {
+	var card = document.createElement('div');
+	card.className = 'card';
+
+	var poster = document.createElement('img');
+	poster.src = itemData.poster;
+	card.appendChild(poster)
+
+	var title = document.createElement('h5');
+	title.innerHTML = itemData.title + " (" + itemData.year + ")"
+	card.appendChild(title);
+
+	var itemGenres = document.createElement('p');
+	itemGenres.innerHTML = 'Genres: ' + itemData.genre.join(', '); // should fix plurals here
+	itemGenres.className = 'capitalize'
+	card.appendChild(itemGenres);
+
+	return card;
+}
+
+function onFilterChange(e) {
+	var filterType = e.target.name; // technically unnecessary, but more readable!
+	var toggledValue = e.target.value;
+
+	if (filterType === 'genre') {
+		var i = FILTER['genres'].indexOf(toggledValue);
+		if (i > -1) {
+			FILTER['genres'].splice(i, 1);
+		} else {
+			FILTER['genres'].push(toggledValue)
+		}
+	}
+
+	if (filterType === 'year') {
+		var i = FILTER['years'].indexOf(toggledValue);
+		if (i > -1) {
+			FILTER['years'].splice(i, 1);
+		} else {
+			FILTER['years'].push(toggledValue)
+		}
+	}
+
+	if (filterType === 'media-type') {
+		FILTER['mediaType'] = toggledValue;
+	}
+
+	// if (filterType === 'some shit')
+	
+	buildAllMediaCards();
+
+}
+
+function resetOneFilter(itemId) {
+	console.log(itemId)
+	console.log(document.getElementById(itemId))
+	document.getElementById(itemId).checked = false;
+}
+
+function resetFilters() {
+	if (FILTER['genres'].length > 0) {
+		FILTER['genres'].forEach(genre => {
+			resetOneFilter(genre);
+		});
+	}
+
+	if (FILTER['years'].length > 0) {
+		FILTER['years'].forEach(year => {
+			resetOneFilter(year);
+		});
+	}
+
+	if (FILTER['mediaType'] !== '') {
+		resetOneFilter('media-' + FILTER['mediaType']);
+	}
+
+	FILTER = { genres: [], years: [], mediaType: '' };
+
+	buildAllMediaCards();
+}
--- submission.md
+++ submission.md
@@ -4,12 +4,36 @@ These notes will be read by HubSpot developers. Drop us a line!
 
 ## Given more time, what would you have done differently?
 
-_your answer here_
+- written tests!!
+
+- separated logic out more thoughtfully. I don't like having all this heavy JS in one file generally, and prefer to separate my logic out. Doesn't seem like a good use of my limited time in this instance though.
+
+- correctly handled singular/plural genre/s
+
+- iterated on the JavaScript structure. You can tell that I started small and built up--the GENRES variable for example. I'm 100% sure that I'm breaking some conventions where I landed; just seemed like the wrong area to focus on.
+
+- generally speaking, i thought more about user interaction than about data structure here. there are a lot of rooms for optimization.
+
+- in the real world, i'd want to talk with product about how they want to handle images of different sizes, fallbacks (I see that sneaky failing image you put in there!), etc. so every "card" looked its best
 
 ## How did you deviate from the directions, if at all, and why?
 
-_your answer here_
+- I did not include a search bar. It was a time issue.
+
+- didn't update the picker to show the number of selections (1 GENRE, 2 YEARS, etc). Again, time.
+
+- many many areas that are not quite to-spec visually--for example, the check boxes on the checkbox dropdown. It is a decent mockup, but not production-ready.
 
 ## Is there anything else you'd like to let us know?
 
-_your answer here_
+- This feels maybe 90% of the way "done" to me, and 85% of the way to a "pull request" level. Example: what is going on with that "form" in the main page?? I added it to take advantage of the semantic "reset" button but really, either we should use the full force of forms or we shouldn't put that in there. A "PR-ready" effort would either be in or out on forms.
+
+- I assumed all the data was good! that is some silly nonsense and not what i would do in a real-world scenario. (also I see that broken image)
+
+- In a real-life situation, I would of course use real image assets for the quotation decoration, and I would spend a lot more time perfecting the placement and making sure it looks clean, attractive, and professional at a number of different breakpoints and devices.
+
+- in a real-life scenario, i would probably use 3 breakpoints--mobile, tablet/small screen (2 columns per row), and large screen. I would also check experience in large screens more closely. I'd be more careful and exact about things like distances--the mobile experience is slightly off-center for the exercise, for example.
+
+- efficiency issues with loading. If I were doing it again, I would use a framework from the beginning. That would allow me to use a framework with opinions on states, that only reloads necessary chunks of the content, for example.
+
+- normally i would make this much DRYer. For example. years and genres do basically the same thing; would be easier/better to have "filter" methods and pass in the type (year vs. genre) instead of these repetitive "if" blocks.
